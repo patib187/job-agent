ffc346efaeb1e7e4e455dbd8651cdcf0a64315d1
patib187 <patibandla187@gmail.com>
2026-06-05 15:23:24 -0700

    Update matcher.py

--- src/job_agent/matcher.py
+++ src/job_agent/matcher.py
@@ -1,1 +1,10 @@
+def ats_score(jd: str, resume: str):
+    jd_words = jd.lower().split()
+    res = resume.lower()
 
+    if not jd_words:
+        return 0
+
+    matches = sum(1 for w in jd_words if w in res)
+
+    return int((matches / len(jd_words)) * 100)
